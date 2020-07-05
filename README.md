a0606ab1b8dded61675411da226932eabfb98ec1
ThameezBo <thameez.bodhanya@allangray.co.za>
2020-07-05 17:47:52 +0200

    feature: add v0.0.2

--- README.md
+++
@@ -1,2 +0,0 @@
-# app-ninety9names-web
-Flutter Web Implementation of ninety9names
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "222a17f9d0799dbe1aed9683f5959f8e",
 "/": "222a17f9d0799dbe1aed9683f5959f8e",
-"main.dart.js": "da5227d3d27e5e974f9bd0fff8dfc41b",
+"main.dart.js": "89e9652a24bec35f62536456578682db",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
